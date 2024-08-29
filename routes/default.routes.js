95f1f1382325543a3d730afb18e8a1d474628787
MarAntBQ <github@marantbq.dev>
2024-08-29 02:31:58 -0500

    refactor: Update welcome message in default route to include API version

--- routes/default.routes.js
+++ routes/default.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     // Return a welcome message in JSON format
-    res.json({ message: 'Welcome to the Marbust Accounts API' });
+    res.json({ message: 'Welcome to the Marbust Accounts API 2.5.0' });
 });
 
 module.exports = router;
